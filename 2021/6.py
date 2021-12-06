8c6b2cbba90f8ba13b9cab38db33e4bdb7736dfe
Alexander Bai <vhurryharry@gmail.com>
2021-12-06 08:49:19 -0500

    added without numpy soluiton

--- 2021/6.py
+++ 2021/6.py
@@ -3,31 +3,43 @@ import numpy as np
 with open('6.in') as infile:
     input = infile.read()
 
-lanternfish = np.array(list(map(int, input.split(','))))
+
+def using_numpy(input):
+    lanternfish = np.array(list(map(int, input.split(','))))
+
+    def init_ages(lanternfish):
+        ages = np.zeros(9, int)
+        for i in range(9):
+            ages[i] = np.count_nonzero(lanternfish == i)
+
+        return ages
+
+    def new_day(ages):
+        ages = np.roll(ages, -1)
+        ages[6] += ages[8]
+
+        return ages
+
+    def count_lanternfish(ages, days):
+        for day in range(days):
+            ages = new_day(ages)
+
+        return np.sum(ages)
+
+    ages = init_ages(lanternfish)
+
+    print(count_lanternfish(ages, 256))
 
 
-def init_ages(lanternfish):
-    ages = np.zeros(9, int)
-    for i in range(9):
-        ages[i] = np.count_nonzero(lanternfish == i)
+def without_numpy(input):
+    lanternfish = list(map(int, input.split(',')))
+    ages = [lanternfish.count(x) for x in range(9)]
 
-    return ages
+    for day in range(1000):
+        ages.append(ages.pop(0))
+        ages[6] += ages[8]
 
-
-def new_day(ages):
-    ages = np.roll(ages, -1)
-    ages[6] += ages[8]
-
-    return ages
+    print(sum(ages))
 
 
-def count_lanternfish(ages, days):
-    for day in range(days):
-        ages = new_day(ages)
-
-    return np.sum(ages)
-
-
-ages = init_ages(lanternfish)
-
-print(count_lanternfish(ages, 256))
+without_numpy(input)
