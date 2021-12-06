87c2c4b23205bc9983686f2038ba6b0397fb0d99
Alexander Bai <vhurryharry@gmail.com>
2021-12-06 07:49:22 -0500

    add problem 6

---
+++ 2021/6.py
@@ -0,0 +1,34 @@
+import numpy as np
+
+with open('6.in') as infile:
+    input = infile.read()
+
+lanternfish = np.array(list(map(int, input.split(','))))
+
+
+def init_ages(lanternfish):
+    ages = np.zeros(9, int)
+    for i in range(9):
+        ages[i] = np.count_nonzero(lanternfish == i)
+
+    return ages
+
+
+def new_day(ages):
+    new_fish = ages[0]
+    ages = np.roll(ages, -1)
+    ages[6] += new_fish
+
+    return ages
+
+
+def count_lanternfish(ages, days):
+    for day in range(days):
+        ages = new_day(ages)
+
+    return np.sum(ages)
+
+
+ages = init_ages(lanternfish)
+
+print(count_lanternfish(ages, 256))
